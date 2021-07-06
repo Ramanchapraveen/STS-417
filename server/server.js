9b5dbcd5bc0f19a4f49fbafc1408c0be21c766a0
Ramanchapraveen <Praveenramancha99@gmail.com>
2021-07-06 16:50:56 -0400

    Revanth : Server code base created

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mysql = require('mysql')
+const myconn = require('express-myconnection')
+
+const cors = require('cors')
+
+const app = express()
+
+app.use(myconn(mysql, {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'demodemo'
+}))
+app.use(cors())
+
+app.use(require('./routes/routes'))
+
+app.listen(9000, () => {
+    console.log('server running on', 'http://localhost:' + 9000)
+})
